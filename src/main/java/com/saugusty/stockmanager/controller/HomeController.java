101a178eb6f530e086a62eb78969dc7ef7883987
Sabrina <sabrina.augusty@gmail.com>
2021-12-08 00:15:42 +0100

    add_homeController

---
+++ src/main/java/com/saugusty/stockmanager/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.saugusty.stockmanager.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	public static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/")
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home!!! The local is {}", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate);
+		
+		return "home";
+	}
+	
+}
